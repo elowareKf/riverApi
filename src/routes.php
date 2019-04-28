761bd12ff5423fcbb30c8cae7b0397bb699c1699
KFischer (mba) <klaus.fischer@eloware.com>
2019-04-28 09:43:44 +0200

    Base REST Get function implemented

--- src/routes.php
+++ src/routes.php
@@ -4,9 +4,29 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+class template{
+    public $message;
+}
+
 return function (App $app) {
     $container = $app->getContainer();
 
+    $app->get('/level/[{name}]' , function (Request $request, Response $response, array $args) use ($container){
+        $data = new template();
+        $data->message = "Hello ".$args['name'];
+
+        return $response->withJson($data);
+    });
+
+    $app->post('/level/[{name}]', function(Request $request, Response $response, array $args){
+
+        $data = $request->getParsedBody();
+
+        $data->server = "Me";
+
+        return $response->withJson($data);
+    });
+/*
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
@@ -14,4 +34,5 @@ return function (App $app) {
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
+*/
 };
