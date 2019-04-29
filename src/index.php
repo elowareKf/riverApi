902bd4fc74f9d26a03cd21a064e0b04b0b8e5b95
KFischer (mbp) <klaus.fischer@eloware.com>
2019-04-29 12:55:17 +0200

    Now REST runs

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@ use Slim\Http\Response;
 
 require 'vendor/autoload.php';
 
-$app = new App();
+$app = new \Slim\App;
 
 class template
 {
@@ -16,22 +16,31 @@ class template
 
 $app->get('/',
     function(Request $request,  Response $response,  array $args){
-    $response->write('Hello World from PHP');
+    $response->write('Hello World from PHP with SLIM');
 });
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
 
 $app->get('/dummy',
-    function(Request $request, Response $response, array $args){
-    $response->write('You are a dummy');
-});
+    function(Request $request,  Response $response,  array $args){
+        $response->write('Hello Dummy from PHP with SLIM');
+    });
 
-$app->get('/level/[{name}]', function (Request $request, Response $response, array $args) {
+
+$app->get('/level/{name}', function (Request $request, Response $response, array $args) {
     $data = new template();
     $data->message = "Hello " . $args['name'];
 
     return $response->withJson($data);
 });
 
-$app->post('/level/[{name}]', function (Request $request, Response $response, array $args) {
+$app->post('/level/{name}', function (Request $request, Response $response, array $args) {
     $measurement = $request->getParsedBody();
 
 
@@ -39,5 +48,6 @@ $app->post('/level/[{name}]', function (Request $request, Response $response, ar
 
     return $response->withJson($measurement);
 });
+
 
 $app->run();
