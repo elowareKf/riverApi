b20474a912aba8146c2d603ce35a17bcd34a32c1
KFischer (mbp) <klaus.fischer@eloware.com>
2019-07-04 20:31:43 +0200

    Enabled CORS

--- src/index.php
+++ src/index.php
@@ -12,6 +12,18 @@ require './routes/RouteSection.php';
 
 $app = new App;
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 
 $app->group('/river', function () use ($app) {
     RouteRiver::route($app);
